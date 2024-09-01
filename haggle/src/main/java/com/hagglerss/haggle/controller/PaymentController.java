728abd396f6263d0de8ee4c8f678826fcf0aae4c
Lani Kim <poikl11234@naver.com>
2024-09-01 19:45:54 +0900

    정적 리소스 showing 에러 해결 / 결제 setting(ing)

--- haggle/src/main/java/com/hagglerss/haggle/controller/PaymentController.java
+++ haggle/src/main/java/com/hagglerss/haggle/controller/PaymentController.java
@@ -1,9 +1,16 @@
 package com.hagglerss.haggle.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/payment")
 public class PaymentController {
+
+    @RequestMapping("")
+    public String movePayment() {
+        return "payment/payment";
+    }
 
 
 }

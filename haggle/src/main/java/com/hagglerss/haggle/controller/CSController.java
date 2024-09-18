bea85d6157e00ca3017013609c59486af79aca71
Lani Kim <poikl11234@naver.com>
2024-09-18 18:40:33 +0900

    [CS] 챗봇 연동

---
+++ haggle/src/main/java/com/hagglerss/haggle/controller/CSController.java
@@ -0,0 +1,17 @@
+package com.hagglerss.haggle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cs")
+public class CSController {
+
+    @RequestMapping("")
+    public String moveMainCS(){
+        return "cs/cs_main";
+    }
+
+
+
+}

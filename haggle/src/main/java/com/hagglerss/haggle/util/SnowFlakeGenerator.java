834732e2c98f9021911db03d8ea67b4bbe1b8d98
Lani Kim <poikl11234@naver.com>
2024-09-18 18:09:28 +0900

    [결제] 주문번호생성 완료

--- haggle/src/main/java/com/hagglerss/haggle/util/SnowFlakeGenerator.java
+++ haggle/src/main/java/com/hagglerss/haggle/util/SnowFlakeGenerator.java
@@ -1,8 +1,6 @@
 package com.hagglerss.haggle.util;
 
-import com.sun.source.tree.IdentifierTree;
 import lombok.NoArgsConstructor;
-import org.springframework.expression.spel.ast.Identifier;
 import org.springframework.stereotype.Component;
 
 import org.hibernate.id.IdentifierGenerator;
@@ -17,18 +15,19 @@ import java.time.Instant;
 @NoArgsConstructor
 public class SnowFlakeGenerator implements IdentifierGenerator {
 
-    private static final int CASE_ONE_BITS = 10;
-    private static final int CASE_TWO_BITS = 9;
-    private static final int SEQUENCE_BITS = 4;
+    private static final int CASE_ONE_BITS = 10;  // 케이스 구분 비트 수
+    private static final int CASE_TWO_BITS = 9; // 케이스 구분 비트 수
+    private static final int SEQUENCE_BITS = 4; // 일련번호 시퀀스 비트 수
 
-    private static final int maxSequence = (int) (Math.pow(2, CASE_ONE_BITS) - 1); // 2^5 -1
+    private static final int maxSequence = (int) (Math.pow(2, CASE_ONE_BITS) - 1); // 2^5 -1 시퀀스의 최대값
 
-    private static final long CUSTOM_EPOCH = 1420070400000L;
-    private volatile long sequence = 0L;
-    private int case_one = 10;
-    private int case_two = 0;
-    private volatile long lastTimestamp = -1L;
+    private static final long CUSTOM_EPOCH = 1420070400000L; // 커스텀 타임스탬프
+    private volatile long sequence = 0L; // 마지막 비트 담당 시퀀스 값
+    private int case_one = 10; // 추가적 케이스 식별자
+    private int case_two = 0; // 추가적 케이스 식별자
+    private volatile long lastTimestamp = -1L; // 마지막 ID 생성 타임스탬프
 
+    /* 엔티티에 대해 ID 요청시 호출됨 */
     @Override
     public Serializable generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) throws HibernateException {
         return nextId();
@@ -38,6 +37,7 @@ public class SnowFlakeGenerator implements IdentifierGenerator {
         return Instant.now().toEpochMilli() - CUSTOM_EPOCH;
     }
 
+    /* 고유 ID 생성 */
     private synchronized long nextId(){
         long currentTimestamp = timestamp();
 
@@ -54,6 +54,7 @@ public class SnowFlakeGenerator implements IdentifierGenerator {
         return makeId(currentTimestamp);
     }
 
+    /* ID 생성 */
     private Long makeId(long currentTimestamp){
         long id = 0;
         id |= (currentTimestamp << CASE_ONE_BITS + CASE_TWO_BITS + SEQUENCE_BITS);
@@ -64,6 +65,7 @@ public class SnowFlakeGenerator implements IdentifierGenerator {
         return id;
     }
 
+    /* 동일 타임스탬프 중복 ID 생성 방지 */
     private long waitNextMillis(long currentTimestamp){
         while (currentTimestamp == lastTimestamp){
             currentTimestamp = timestamp();
